b7f094a54d021a9e12c73c2462416ac86ae063cd
ISSA <ISSA@ISSA>
2020-01-14 16:21:13 +0100

    this is my first program1

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int _myValue =7;    
+                    
+          Console.Write("{0}", _myValue=7);
+        }
+    }
+}
